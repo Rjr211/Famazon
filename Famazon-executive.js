35f30405e78012b517d807cd41070f8507a6f8da
Rob <rjr211@scarletmail.rutgers.edu>
2018-08-31 18:02:57 -0400

    prompt fix

--- Famazon-executive.js
+++ Famazon-executive.js
@@ -8,3 +8,32 @@ var connection = mysql.createConnection({
 	password: '',
 	database: 'Famazon', 
 });
+
+ar newDept = [];
+
+
+connection.connect();
+
+//creates the question that will be prompted to the user
+var executiveOptions = {
+	properties:{
+		eOptions:{
+			description: colors.blue('Key in one of the following options: 1) View Product Sales by Department 2) Create New Department')
+		},
+	},
+};
+
+prompt.start();
+
+//gets the information responded by the user from the prompt
+prompt.get(executiveOptions, function(err, res){
+	//this explains what should be done based on what the user answered to the prompt
+	if(res.eOptions == 1){
+		viewProductSales();
+	} else if(res.eOptions == 2){
+		createDepartment();
+	} else{
+		console.log('You picked an invalid choice!');
+		connection.end();
+	}
+});
