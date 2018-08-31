17ed7e397acbc576374e11174a6f72ec33fbfbe9
Rob <rjr211@scarletmail.rutgers.edu>
2018-08-31 19:11:52 -0400

    Manager branch created

---
+++ Famazon-Manager.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var prompt = require('prompt');
+var colors = require('colors/safe');
+var Table = require('cli-table');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'Famazon', 
+});
+
+var inventoryUpdate = [];
+var addedProduct = [];
+
+connection.connect();
+
+// * List a set of menu options: 1) View Products for Sale 2) View Low Inventory 3) Add to Inventory 4) Add New Product
+
+//creates the prompt that will be loaded when the app loads
+var managerOptions = {
+	properties:{
+		mOptions:{
+			description: colors.blue('Key in one of the following options: 1) View Products for Sale 2) View Low Inventory 3) Add to Inventory 4) Add New Product')
+		},
+	},
+};
--- Famazon-customer.js
+++ Famazon-customer.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
-	password: 'r0binson',
+	password: '',
 	database: 'Famazon', 
 });
 
