17fb21772629e069d02fc0acb2caf80f9cb22b2b
Rob <rjr211@scarletmail.rutgers.edu>
2018-08-31 17:49:13 -0400

    executive branch added

---
+++ Famazon-executive.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+var prompt = require('prompt');
+var colors = require('colors/safe');
+var Table = require('cli-table');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'Famazon', 
+});
